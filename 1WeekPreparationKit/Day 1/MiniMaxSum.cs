73e62dd620d72484b751c4848167b701d32b5bcd
Sueda Turgut <suedat24@gmail.com>
2024-02-11 00:00:31 +0300

    MiniMaxSum created

---
+++ 1WeekPreparationKit/Day 1/MiniMaxSum.cs
@@ -0,0 +1,36 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long sum = arr.Sum(num => (long)num);
+        long min = sum - arr.Max(num => (long)num);
+        long max = sum - arr.Min(num => (long)num);
+        Console.WriteLine($"{min} {max}");
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
