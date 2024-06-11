eb71a7bb59171052d7d938a592069b7aa24ca8f3
Sueda Turgut <suedat24@gmail.com>
2024-06-11 23:45:47 +0300

    deleted

--- SQL/Revising the Select Query I/Revising the Select Query II/Solution.sql
+++
@@ -1,9 +0,0 @@
-﻿/* Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
-The CITY table is described as follows:
-
-https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg */
-
-SELECT NAME
-FROM CITY
-WHERE CountryCode = 'USA'
-AND POPULATION >120000;
